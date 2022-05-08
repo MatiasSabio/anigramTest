f45c6f40a893ad8b5c67d73bc4f811572a2b8d8f
Matias Sabio <matiassabio10@gmail.com>
2022-05-08 18:08:18 -0300

    end configure

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js",
+    filename: 'app.bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
-  mode: "development",
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node-modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-};
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
